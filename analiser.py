afa66b8c74598d62521d24209f202d181e545cc4
jpulup <jpulup@gmail.com>
2024-05-26 17:33:19 -0300

    comentado

--- analiser.py
+++ analiser.py
@@ -5,7 +5,8 @@ from control.matlab import *
 import numpy as np
 import control
 
-
+# faz uma interpolação entre array[i-1] e array[i+i] para valores de array[i] que estão acima 
+#   do valor maximo passado
 def errorDelete(signal,max):
     for i in range(len(signal)):
         if i>0 and i<len(signal)-1:
@@ -13,92 +14,37 @@ def errorDelete(signal,max):
                 signal[i]=(signal[i-1]+signal[i+1])/2
     return signal
 
-with open("dados4.json") as f:
-    fs=json.load(f)[2:]
-    fs[0]=0.000000000001
-
+# as informações necessárias para acessar todos os arquivos .json necessarios para cada frequencia
 pathsExtentions=["","input","tempos"]
 pathsExtentions=["Hz"+extention+".json" for extention in pathsExtentions]
 
-print("ex:",pathsExtentions)
-
-for freq in [0.1,1,2.4,4,10,100,1000]:
-    values=[]
-    for extention in pathsExtentions:
-        print("freq:",freq,"extention:",extention)
-        print("path:","freq"+str(freq)+extention)
-        values.append(json.load(open("freq"+str(freq)+extention)))
+# faz um loop para avaliarmos as respostas em frequencia para cada frequencia coletada
+for freq in [0,0.1,1,2.4,4,10,100,1000]:
+    values=[] # será uma lista com 3 listas, cada uma com valores para o tempo,entrada e saida
+    # preenchimento da lista de listas de valores
+    for extention in pathsExtentions: values.append(json.load(open("freq"+str(freq)+extention)))
+    # plot dos valores da entrada, como esses valores são de um seno que é usado para calcular um
+    #   um valor de duty cycle entre 50 e 250 de um PWM que tem valor maximo de duty cycle de 255,
+    #   precisamos fazer esses calculos e multiplicar por 3.3(tensão maxima do PWM) para saber qual
+    #   a tensão de entrada estava sendo mandada naquele momento
     plt.plot(values[2],[(3.3*(200*(((y*10e3)+1)/2)+50)/255) for y in values[1]],'--',label="Entrada")
+    # para algumas frequencias usamos o 'errorDelete'(com max variavel) para melhorar a onda, outras não
+    # aqui o uso do errorDelete com max=9 só serve para freq=4, para analizar outras freqs, é preciso mudar esse valor
+    # multiplicamos os valores por 10e6/950 pq precisou, n sei pq
     values[0]=errorDelete([((y*10e6))/950 for y in values[0]],9)
-    for _ in range(5):
-        values[0]=errorDelete(values[0],9)
+    # caso queira usar o errorDelete vária vezes:
+    """for _ in range(5):
+        values[0]=errorDelete(values[0],9)"""
+    # plot da saída original
     plt.plot(values[2],values[0],label='Saída original')
+    # plot da saída filtrada
     xs,smooth=smoothSignal([values[2],values[0]],0.2,25)
     xs,smooth=smoothSignal([xs,smooth],1,10)
     plt.plot(xs,smooth,label='Saída filtrada')
+    # matplotlib stuff
     plt.xlabel("Tempo (s)",fontweight='bold')
     plt.ylabel("Frequência (Hz)",fontweight='bold')
-    #plt.plot([0,20],[4.92,4.92],'--',c='r')
-    #plt.plot([0,10],[2.275+1.32,2.275+1.32],'--',c='r')
-    #plt.plot([0,10],[3.42+1.32,3.42+1.32],'--',c='r')
-    #plt.plot([0,10],[3.528+1.32,3.528+1.32],'--',c='r')
-    """ts=[0.0455]
-    cs=['b','r','g','k']
-    k=2.79
-    plt.show()
-    for t,c in zip(ts,cs):
-        s=control.TransferFunction.s
-        tf=k/((t*s)+1)
-        ti,y=control.step_response(tf)
-        ti=[time+2.01 for time in ti]
-        y=[(Y*1.29)+1.32 for Y in y]
-        plt.plot(ti,y,c=c,label="T="+str(t))"""
     plt.title("Frquência de entrada = "+str(freq)+" Hz")
     plt.legend()
     plt.grid(True)
     plt.show()
-    
-ts=[1/f for f in fs]
-
-a=0
-ms=[]
-for t in ts:
-    ms.append((((t+a)*10e-6)-10e6)/2)
-    a+=t
-
-                
-fcs=[val/2 for val in errorDelete(fs,0.0005)]
-
-ts[0]=0
-
-smuls=[1,3]
-nneis=[10]
-cols=['r','g','y','b']
-
-fcs=[f*100000 for f in fcs]
-
-def plotLine(v): plt.plot([0,3.2],[v,v])
-
-x,y=smoothSignal([ms,fcs],1,10)
-plt.plot(x,y)
-for tau in [0.2211,0.174,0.1637,0.1864]:
-    num=[23.3333/2.94]
-    den=[tau,1]
-    sys=tf(num, den)
-
-    control.bode(sys,dB=True)
-    plt.show()
-
-    y,t=step(sys)
-    y,t,_=lsim(sys,2.94,np.linspace(0,3,500))
-    plt.plot(t,y)
-plt.show()
-
-plotLine(23.333)
-plotLine(23.333*0.632)
-plotLine(23.333*0.95)
-plotLine(23.333*0.98)
-for f,c in zip(nneis,cols):
-    x,y=smoothSignal([ms,fcs],1,f)
-    plt.plot(x,y,c)
-plt.show()
--- reader.py
+++ reader.py
@@ -2,40 +2,52 @@ import serial
 import matplotlib.pyplot as plt
 import json
 
+# definne a porta COM do pc/notebook que será usada
 sensores = serial.Serial('COM6', 115200)
 
-ts=[]
+# listas de valores a serem gravados
+periodos=[]
 tempos=[]
 entrada=[]
 
+# qual a freqência de entrada a ser coletada
 freq=0
+
+# dependendo da frequencia de entrada, pegamos um numero de pontos diferente, pois precisamos de uma quantidade
+#   mais ou menos fixa de picos de onda. Chegamos a esse método por testes experimentais
+if freq==0: nPoints=1000
+else: nPoints=int(max(1000/freq,1000))
 
 #If the program doesnt recieve a string the code will pass the info
 while(sensores.inWaiting()==0):
     pass
 #Capting the ESP32 string and converting it into a list 
-for t in range(int(max(1000,1000))):
+for t in range(nPoints):
     leitura = sensores.readline()
     leitura = str(leitura,'utf-8')
     leitura = leitura.strip('\r\n')
     valores = leitura.split(',')[0]
+    # Se o ESP mandar uma mensagem com 'Vel', significa que tem valores a serem gravados
     if "Vel " in valores:
         lista=valores.split("Temp ")
         protoList=lista[1].split("Seno ")
-        lista=[lista[0].split("Vel ")[1],protoList[0],protoList[1]]
+        lista=[lista[0].split("Vel ")[1],protoList[0],protoList[1]] # lista com os 3 valores a serem gravados
+        # print para checar o funcionamenteo
         print(lista)
-        ts.append(float(lista[0]))
+        # salva os valores nas listas
+        periodos.append(float(lista[0]))
         tempos.append(float(lista[1])/1000000)
-        entrada.append(float(lista[2])/10000)
+        entrada.append(float(lista[2]))
 
-fs=[1/t for t in ts]
+# Converte periodos em frequencias
+frequencias=[1/t for t in periodos]
 
-print("Todos:",ts)
-plt.plot(tempos,fs)
+# plot dos valores recebidos para inspeção, antes de serem salvos
+plt.plot(tempos,frequencias)
 plt.plot(tempos,entrada)
-#plt.axis([0,1000/freq,-0.05,0.005])
 plt.show()
 
-with open("freq"+str(freq)+"Hz.json",'w') as f: json.dump(fs,f)
+# Salvamos os valores
+with open("freq"+str(freq)+"Hz.json",'w') as f: json.dump(frequencias,f)
 with open("freq"+str(freq)+"Hztempos.json",'w') as f: json.dump(tempos,f)
 with open("freq"+str(freq)+"Hzinput.json",'w') as f: json.dump(entrada,f)
